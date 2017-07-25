4674066e4d378dbf91405d35ec7d1b453446070e
Tejash Shrestha <Tejash Shrestha>
2017-07-25 11:37:58 +0530

    managed new record and old record update in hw

--- THEPTA/src/main/java/com/sakha/thepta/dao/impl/HomeworkDaoImpl.java
+++ THEPTA/src/main/java/com/sakha/thepta/dao/impl/HomeworkDaoImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
@@ -23,8 +24,25 @@ public class HomeworkDaoImpl implements HomeworkDao {
 	
 	@Override
 	public int giveHomeworkBysubjectId(HomeworkModel newHomeworkModel) {
+		Criteria criteria = sessionFactory.getCurrentSession().createCriteria(HomeworkModel.class);
+		criteria.add(Restrictions.eq("classId", newHomeworkModel.getClassId())).add(Restrictions.eq("sectionId", newHomeworkModel.getSectionId())).add(Restrictions.eq("teacherId", newHomeworkModel.getTeacherId()));
 		Session session  = sessionFactory.getCurrentSession();
-		int result = (Integer)session.save(newHomeworkModel);
+		int result;
+		if (criteria.list().size()==0) {		
+		result = (Integer)session.save(newHomeworkModel);
+		}
+		else {
+			
+			String hql = "update HomeworkModel set homeworkText = ?, homeworkFileContent = ?, date = ? where classId = ? AND sectionId = ? AND teacherId = ?";
+			Query query = session.createQuery(hql);
+			query.setParameter(0, newHomeworkModel.getHomeworkText());
+			query.setParameter(1, newHomeworkModel.getHomeworkFileContent());
+			query.setParameter(2, newHomeworkModel.getDate());
+			query.setParameter(3, newHomeworkModel.getClassId());
+			query.setParameter(4, newHomeworkModel.getSectionId());
+			query.setParameter(5, newHomeworkModel.getTeacherId());
+			result = (Integer)query.executeUpdate();			
+		}
 		return result;
 	}
 
